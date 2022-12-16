020307aa1e6606577c6b411fe169115908b5fd61
annasatanina <anna.satanina@gmail.com>
2022-12-17 12:46:25 +1300

    animation on notification widget and stylings changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,20 +16,17 @@ const Home: React.FC = () => {
   useEffect(() => {
     const notication = notificationRef.current;
     const noticationIcon = notificationIconRef.current;
+    const timeline = gsap.timeline({ 
+      defaults: { duration: 0.2, ease: "Power.out3" }
+    })
 
-    gsap.fromTo(
-      noticationIcon,
-      {
-        opacity: 0,
-        scale: 1.3,
-      },
-      {
-        delay: 3,
+    timeline
+      .to(['.notification-panel', '.notification-title', notication, noticationIcon], {
         opacity: 1,
-        scale: 1,
-        ease: "Power.out3",
-      }
-    );
+        stagger: 0.3,
+      }, "<+0.2")
+      .to(noticationIcon, { scale:1.3, ease: "power4.out" })
+      .to(noticationIcon, { scale: 1 })
 
     return () => {
       gsap.killTweensOf(notication);
@@ -40,7 +37,7 @@ const Home: React.FC = () => {
 
   return (
     <div
-      className={"relative flex flex-col justify-start items-center h-screen"}
+      className={"relative flex flex-col justify-start items-center h-screen max-w-4xl mx-auto"}
     >
       <div
         className={"w-full h-fit flex justify-center items-center px-20 py-10"}
@@ -48,13 +45,13 @@ const Home: React.FC = () => {
         <div
           ref={notificationRef}
           className={
-            "min-w-fit w-full bg-gray-200 rounded-full px-4 py-2 flex cursor-pointer justify-center items-center space-x-2 hover:shadow-md relative text-2xl"
+            "notification-panel opacity-0 min-w-fit w-full bg-white rounded-full px-4 py-3 flex cursor-pointer justify-center items-center space-x-2 hover:shadow-md relative text-2xl"
           }
           onClick={() => setIsOpen(!isOpen)}
         >
           <div className={"inline-flex relative items-center"}>
-            <p>Notification</p>
-            <div ref={notificationIconRef} className={"absolute -right-10"}>
+            <p className="notification-title opacity-0">Notification</p>
+            <div ref={notificationIconRef} className={"absolute -right-10 opacity-0"}>
               <FiMail />
               <span className={"absolute -top-1.5 text-xs -right-2"}>
                 {notificationCount}
@@ -65,13 +62,13 @@ const Home: React.FC = () => {
       </div>
       <div
         className={
-          "w-full h-fit flex content-end items-start justify-center py-10 px-20"
+          "w-full h-fit flex content-end items-start justify-center py-2 px-20"
         }
       >
-        <div className={"bg-gray-200 w-full h-full rounded-lg p-2 h-60"}>
-          <p>LIST OF MESSAGES</p>
+        <div className={"bg-white w-full h-full rounded-lg p-2 h-60"}>
+          <p className={"text-center font-bold p-3"}>LIST OF MESSAGES</p>
           <ul>
-            <li>message 1</li>
+            <li className={"hover:bg-slate-100 hover:text-sky-600 p-2 rounded-lg cursor-pointer"} >message 1</li>
             <li>message 2</li>
             <li>message 3</li>
             <li>etc</li>
